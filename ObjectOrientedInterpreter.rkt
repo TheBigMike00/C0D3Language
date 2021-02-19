b74d95521d90b5d13da69b3ef355afd44729dbb5
Michael Brocker <49590891+TheBigMike00@users.noreply.github.com>
2021-02-19 16:37:20 -0600

    final commit

--- ObjectOrientedInterpreter.rkt
+++ ObjectOrientedInterpreter.rkt
@@ -2,6 +2,23 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname ObjectOrientedInterpreter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;global-env is a list of scopes where a scope is a list of name/value pairs (2-lists)
+(define apply-env
+  (lambda (env name)
+    (cond
+      ((null? env) #f)
+      ((eq? (car (car env)) name) (cadr (car env)))
+      (else (apply-env (cdr env) name)))))
+
+;extractors
+(define class->super
+  (lambda (class)
+    (cadr class)))
+
+(define class->env
+  (lambda (class)
+    (car (caddr class))))
+
+
 ;constructors
 (define class
   (lambda (base-class list-of-field-names list-of-field-values list-of-method-names list-of-method-values)
@@ -14,64 +31,65 @@
              (global-env (list (create-pairs-for-scope (append list-of-field-names list-of-method-names)
                                                        (append list-of-field-values list-of-method-values)))))
       (list 'class base-class global-env))))
+
 (define empty-object
   (lambda ()
-    (class '(no-parent) '() '() '(getValue) (list (lambda(var-name) (list var-name 'it-works))))))
-;getters
-;env will be a list of classes
-;start more local and then bubble out
-;sendMessage is going to take in a object,
-;then we will need to pass it the whole person
-;first we call obj->child
-;we look
-;if it is not there we look at the parent
-;extractors
-(define class->super
-  (lambda (class)
-    (cadr class)))
-(define class->list-of-fields
-  (lambda (class)
-    (caddr class)))
+    (class '(no-parent) '() '() '(getValue) (list (lambda (self var-name)
+                                                    (letrec ((checkParent (lambda (theParent var-name)
+                                                                            (let ((answer (apply-env (class->env theParent) var-name))
+                                                                                  (parent (class->super theParent)))
+                                                                              (cond
+                                                                                ((not (eq? answer #f)) answer)
+                                                                                ((and
+                                                                                   (eq? answer #f)
+                                                                                   (not (eq? parent '(no-parent))))
+                                                                                 (checkParent parent var-name))
+                                                                                (else #f))))))
+                                                      (checkParent self var-name)))))))
+                                                          
+                                                    
+(define sendMessageMapper
+  (lambda (class-instance list-of-params)
+    (cond
+      ((null? list-of-params) '())
+      (else (cons (sendMessageHelper class-instance (car list-of-params))
+                  (sendMessageMapper class-instance (cdr list-of-params)))))))
 
-(define sendMessage
-  (lambda(object param)
-    (cond
-      ((not(eq?(searchMethod(objLookDeeperChild(obj->child object)) param) #f))(searchMethod(objLookDeeperChild(obj->child object))param))
-      (else(searchMethod(objLookDeeperParent(obj->parent object)param))))))
-(define searchMethod
-  (lambda(messedUpObject param)
-    (cond
-      ((null? messedUpObject)#f)
-      ((eq? (car (car messedUpObject)) param)(car(cdr(car messedUpObject))))
-      (else (searchMethod (cdr messedUpObject) param))))) 
-                             
-;pass this in the obj->child method
-(define obj->parent
-  (lambda(object)
-    (car(cdr object))))
-(define obj->child
-  (lambda(object)
-    (car(cdr(cdr object)))))
-(define objLookDeeperParent
-  (lambda(object)
-    (caadr(cdr object))))
-(define objLookDeeperChild
-  (lambda(personClass)
-    (car personClass)))
+(define sendMessageHelper
+  (lambda (class-instance message)
+    (let* ((env (class->env class-instance))
+           (answer (apply-env env message))
+           (parent (class->super class-instance))) 
+      (cond
+        ((not (eq? answer #f)) answer)
+        ((and
+          (eq? answer #f)
+          (not (eq? parent '(no-parent)))) (sendMessageHelper parent message))
+        (else (display "Message not found!!"))))))
     
+(define sendMessage
+  (lambda (class-instance message list-of-parameters)
+    ((sendMessageHelper class-instance message) class-instance (car list-of-parameters))))
+      
 (define Person
   (lambda (list-of-field-names list-of-values)
     (class (empty-object) list-of-field-names list-of-values '() '())))
+
 (define p1 (Person '(fname lname age) '(Mike Litman 21)))
 (define p2 (Person '(fname lname age) '(Dave Smith 18)))
-p2
-;(sendMessageHelper (objLookDeeper(obj->parent p2)))
-(obj->parent p2)
-;(car( car ( car (cdr (cdr (car (cdr p2)))))))
-(cadr(objLookDeeperChild(obj->child p2)))
-;(cadr(car(obj->child p2)))
-;(car(car(objLookDeeperChild(obj->child p2))))
-;(searchMethod(objLookDeeperChild(obj->child p2)) 'lname)
-(sendMessage p2 'fname)
-;'child
-;(obj->parent p2)
+(sendMessage p2 'getValue '(age))
+
+(define code '(class Person (fname lname age)))
+;(class (empty-object) list-of-field-names list-of-values '() '())))
+(define parser
+  (lambda (code)
+    (let (type (car code))
+      (cond
+        ((eq? type class) (class (class->super cadr code) (caddr code) '() '() '())))))
+
+(parser code)
+
+
+
+
+
